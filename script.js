c34fc6fffd091d2a625f72c36b8c6d3452d17258
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2023-02-22 17:44:36 +0530

    Submit

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+//your code here
+var todoValue = document.getElementById("newTodoInput");
+var btn = document.getElementById("addTodoBtn");
+var ol = document.getElementById("todoList");
+
+function addTodo() {
+	if (todoValue.value){
+		var li = document.createElement("li");
+		li.textContent = todoValue.value;
+		ol.appendChild(li);
+		todoValue.value="";
+	}
+}
+btn.addEventListener("click",addTodo);
+
 
